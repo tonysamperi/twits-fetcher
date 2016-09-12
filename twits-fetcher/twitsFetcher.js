77e1eae6f096b56023dab42f70e599daac7e65bb
Tony Samperi <anto_s.89@hotmail.it>
2016-09-12 13:19:22 +0200

    Wrong month fixed

--- twits-fetcher/twitsFetcher.js
+++ twits-fetcher/twitsFetcher.js
@@ -79,7 +79,7 @@
         var month = d.getMonth();
         var months = {
             it: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
-            en: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dec"],
+            en: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
             es: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
         };
         if (!!months[lang] && !!months[lang][month])
